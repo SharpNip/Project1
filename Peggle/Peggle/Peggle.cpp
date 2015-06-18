a5c836852646823ca5e3d0f7b278923e62025a17
Sharpnip <1430047@apps.bart.qc.ca>
2015-06-18 09:54:53 -0400

    Initial commit

---
+++ Peggle/Peggle/Peggle.cpp
@@ -0,0 +1,37 @@
+#include "Peggle.h"
+#include "Sprite.h"
+#include "ResourceIDs.h"
+
+Peggle::Peggle()
+{
+	Textures->LoadTexture(Texture::ELEPHANT_DE_MER, "Sprites/Basket.jpg");
+
+
+	Sprite* s = new Sprite(Texture::ELEPHANT_DE_MER);
+
+}
+
+Peggle::~Peggle()
+{
+
+}
+
+void Peggle::Start()
+{
+
+}
+
+void Peggle::Update()
+{
+
+}
+
+void Peggle::Draw()
+{
+
+}
+
+void Peggle::Stop()
+{
+
+}
